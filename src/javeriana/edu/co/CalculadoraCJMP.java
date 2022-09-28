9f3885fed50ddb45d34d9563c11e4a45c593fea7
Kahsma <cajomapa@gmail.com>
2022-09-27 21:31:23 -0500

    Termine El ejercicio, hice unas pruebas en main

---
+++ src/javeriana/edu/co/CalculadoraCJMP.java
@@ -0,0 +1,54 @@
+package javeriana.edu.co;
+
+public class CalculadoraCJMP implements ICalculator {
+
+    @Override
+    public double add(double a, double b) {
+        double suma;
+        suma = a + b;
+        return suma;
+    }
+
+    @Override
+    public double substract(double a, double b) {
+        double diff;
+        diff = a - b;
+        return diff;
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+        double product;
+        product = a * b;
+        return product;
+
+    }
+
+    @Override
+    public double divide(double a, double b) {
+        double dividir = 0;
+
+        try {
+
+            dividir = a / b;
+            if (b == 0) {
+                throw new ArithmeticException("Division por 0");
+
+            }
+
+        } catch (ArithmeticException e) {
+            System.out.println("Ocurrio una exception: " + e.getMessage());
+
+        }
+        return dividir;
+
+    }
+
+    @Override
+    public String about() {
+        String about = "Calculator implemnted by: " + Authors.CJMP;
+
+        return about;
+    }
+
+}
--- src/javeriana/edu/co/Main.java
+++ src/javeriana/edu/co/Main.java
@@ -2,9 +2,11 @@ package javeriana.edu.co;
 
 public class Main {
 	public static void main(String args[]) {
-		//ICalculator calc = new CalculatorCAPA();
-		//calc.divide(20/0);
-		//calc.about();		
+		ICalculator calc = new CalculadoraCJMP();
+		System.out.println(calc.add(20, 2));
+		calc.divide(20, 0);
+		System.out.println(calc.about());
+
 	}
 
 }
