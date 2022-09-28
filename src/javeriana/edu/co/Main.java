9728e9805c0655da5b5010b13946ba9ee8727aff
Kahsma <cajomapa@gmail.com>
2022-09-27 23:11:57 -0500

    revision y otras cosas

--- src/javeriana/edu/co/Main.java
+++ src/javeriana/edu/co/Main.java
@@ -1,22 +1,11 @@
 package javeriana.edu.co;
 
 public class Main {
-	public static void main(String args[]) 
-	{
+	public static void main(String args[]) {
 		ICalculator calc = new CalculadoraCJMP();
 		System.out.println(calc.add(20, 2));
 		calc.divide(20, 0);
 		System.out.println(calc.about());
-<<<<<<< HEAD
-	
-		System.out.println("- - - - - - - -");
-		
-		ICalculator CalculadoraALVA = new CalculadoraALVA();
-		System.out.println (CalculadoraALVA.add(10, 2));
-		CalculadoraALVA.divide (20, 0);
-		System.out.println (CalculadoraALVA.about());
-=======
 		System.out.println(calc.multiply(30, 3));
->>>>>>> e77d8f6d0256405639d275f87d246db036f8acc7
 	}
 }
