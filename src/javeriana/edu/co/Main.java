eac63953a0f6af1f7af167ceef7149c926ff40a9
Betico1928 <betoveluis@gmail.com>
2022-09-27 23:25:13 -0500

    Implemented ALVACalculator at main

--- src/javeriana/edu/co/Main.java
+++ src/javeriana/edu/co/Main.java
@@ -1,11 +1,20 @@
 package javeriana.edu.co;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		ICalculator calc = new CalculadoraCJMP();
 		System.out.println(calc.add(20, 2));
 		calc.divide(20, 0);
 		System.out.println(calc.about());
 		System.out.println(calc.multiply(30, 3));
+
+		System.out.println("- - - - - -");
+		
+		ICalculator ALVACalculator = new CalculadoraALVA();
+		System.out.println("Resultado Suma: " + ALVACalculator.add(50, 10));
+		System.out.println("Resultado Resta: " + ALVACalculator.substract(50, 10));
+		System.out.println("Resultado Multiplicacion: " + ALVACalculator.multiply(50, 10));
+		System.out.println("Resultado Division: " + ALVACalculator.divide(50, 0));
 	}
 }
