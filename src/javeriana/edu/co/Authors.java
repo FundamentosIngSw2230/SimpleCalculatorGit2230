2e1e37ea9a8036302426c4033c985e721bf6d36c
Maria Jose Gomez <mariagomezg@javeriana.edu.co>
2022-09-27 21:43:34 -0500

    CambiandoNombre

--- src/javeriana/edu/co/Authors.java
+++ src/javeriana/edu/co/Authors.java
@@ -4,4 +4,5 @@ public class Authors {
 	public static final String CAPA = "Carlos Parra";
 	public static final String PIKA = "Pikachu";
 	public static final String CJMP = "Camilo Martinez";
+	public static final String MJGG = "Majo Gomez";
 }
