c856bb422cbf60dc9cc9a0775a871e50f0cb0f73
Betico1928 <betoveluis@gmail.com>
2022-09-27 22:40:50 -0500

    Added author "ALVA"

--- src/javeriana/edu/co/Authors.java
+++ src/javeriana/edu/co/Authors.java
@@ -5,4 +5,5 @@ public class Authors {
 	public static final String PIKA = "Pikachu";
 	public static final String CJMP = "Camilo Martinez";
 	public static final String MJGG = "Majo Gomez";
+	public static final String ALVA = "Alberto Vigna";
 }
