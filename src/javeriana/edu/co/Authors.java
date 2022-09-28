86f01d596ac555e57afa70da5b408258963478b7
Kahsma <cajomapa@gmail.com>
2022-09-27 20:42:11 -0500

    added my name

--- src/javeriana/edu/co/Authors.java
+++ src/javeriana/edu/co/Authors.java
@@ -3,4 +3,5 @@ package javeriana.edu.co;
 public class Authors {
 	public static final String CAPA = "Carlos Parra";
 	public static final String PIKA = "Pikachu";
+	public static final String CJMP = "Camilo Martinez";
 }
